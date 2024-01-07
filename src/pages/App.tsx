405da151802d1719527e198445d98461e37e00ee
Trevor McCasland <trevor@DESKTOP-D6015RP.(none)>
2024-01-06 19:07:01 -0600

    remove unused pages and use mui for grid layout

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -14,82 +14,16 @@ import {
   useHistory,
   useLocation,
 } from 'react-router-dom'
-import { ProvideAuth, useAuth } from './UseAuth'
 import Home from './Home'
 
 export default function App() {
   return (
-    <ProvideAuth>
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            <Welcome />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <PrivateRoute path="/home">
-            <Home />
-          </PrivateRoute>
-        </Switch>
-      </Router>
-    </ProvideAuth>
-  )
-}
-
-function PrivateRoute({ children, ...rest }: RouteProps) {
-  const auth = useAuth()
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        auth.user ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: '/login',
-              state: { from: location },
-            }}
-          />
-        )
-      }
-    />
-  )
-}
-
-function Welcome() {
-  return (
-    <>
-      <h1>Welcome</h1>
-      <Link to="/home">Home</Link>
-    </>
-  )
-}
-
-function Login() {
-  const [disabled, setDisabled] = useState(false)
-  const history = useHistory()
-  const location = useLocation()
-
-  const auth = useAuth()
-
-  // @ts-ignore
-  const { from } = location.state || { from: { pathname: '/home' } }
-
-  async function login() {
-    if (disabled) return
-    setDisabled(true)
-    await auth.signIn()
-    history.replace(from)
-  }
-
-  return (
-    <div>
-      <h1>Login</h1>
-      <button disabled={disabled} onClick={login}>
-        login
-      </button>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,22 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import store, { counterSlice } from '@/stores'
 import { RootState } from '@/types'
+import Grid from '@mui/material/Unstable_Grid2'
+import { Button, ButtonProps } from '@mui/material'
+
+const TttButton = ({ sx, children }: ButtonProps) => (
+  <Button
+    variant="text"
+    sx={{
+      '&:hover': { backgroundColor: 'transparent' },
+      borderRadius: 0,
+      border: 1,
+      borderColor: 'text.primary',
+      ...sx,
+    }}>
+    {children}
+  </Button>
+)
 
 export default function Home() {
   const count = useSelector<RootState>((state) => state.counter.count) as number
@@ -20,6 +36,25 @@ export default function Home() {
         </button>
         <button onClick={() => store.dispatch(counterSlice.actions.decrease(1))}>decrease</button>
       </div>
+      <Grid container direction="column">
+        <Grid container>
+          <TttButton disabled={true} sx={{ borderTop: 0, borderLeft: 0 }}>
+            1
+          </TttButton>
+          <TttButton sx={{ borderTop: 0 }}>2</TttButton>
+          <TttButton sx={{ borderTop: 0, borderRight: 0 }}>3</TttButton>
+        </Grid>
+        <Grid container>
+          <TttButton sx={{ borderLeft: 0 }}>1</TttButton>
+          <TttButton>2</TttButton>
+          <TttButton sx={{ borderRight: 0 }}>3</TttButton>
+        </Grid>
+        <Grid container>
+          <TttButton sx={{ borderBottom: 0, borderLeft: 0 }}>1</TttButton>
+          <TttButton sx={{ borderBottom: 0 }}>2</TttButton>
+          <TttButton sx={{ borderBottom: 0, borderRight: 0 }}>3</TttButton>
+        </Grid>
+      </Grid>
     </div>
   )
 }
