4ed36e701316697b98cfcb390e8d7212cbddfe57
Trevor McCasland <trevormccasland1@gmail.com>
2024-01-08 00:09:05 -0600

    center line and create a wait effect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@
  * Date: 2021-07-24 12:14 (GMT+0900)
  */
 import { Button, Paper } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Home.css'
 import { motion } from 'framer-motion'
@@ -25,13 +25,15 @@ const draw = {
 }
 
 export default function Home() {
+  const [playTime, setPlayTime] = useState(false)
+  setTimeout(() => setPlayTime(true), 5000)
   return (
     <Paper className="homeContainer">
       <div className="homeCenter">
-        <Button component={Link} to="/tictactoe">
+        <Button disabled={!playTime} component={Link} to="/tictactoe">
           Play
         </Button>
-        <div>
+        <div style={{ marginLeft: '20px' }}>
           <motion.svg fill="white" width="600" height="600" viewBox="0 0 600 600" initial="hidden" animate="visible">
             <motion.circle cx="100" cy="100" r="80" stroke="#ff0055" variants={draw} custom={1} />
             <motion.line x1="220" y1="30" x2="360" y2="170" stroke="#00cc88" variants={draw} custom={1} />
